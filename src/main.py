31aef7cf16243b0ff0d56c8d2da1e7d352e5109d
wi-photos <53404933+christislord12@users.noreply.github.com>
2022-01-19 22:01:48 -0600

    Version 1.3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from panda3d.core import loadPrcFileData
-loadPrcFileData("", "sync-video t")
-loadPrcFileData("", "show-frame-rate-meter f")
 loadPrcFileData("", "win-size 800 600")
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
@@ -12,80 +10,90 @@ import sys
 class Game(DirectObject):
     def __init__(self):
         base = ShowBase()
-        # text holding node
-        self.TextHoldingNode = TextNode("node name")
-        self.TextHoldingNode.setText("")
-        self.TextHolder = aspect2d.attachNewNode(self.TextHoldingNode)
-        self.TextHolder.reparentTo(render)
-        self.TextHoldingNode.setTextColor(0,1,0,1)
-        self.BookLoad("Dedicatory");
-        # Camera
-        base.useTrackball()
-        base.trackball.node().setPos(-7.55,0,6)
-        # Controls
-        self.forward_button = KeyboardButton.up()
-        self.backward_button = KeyboardButton.down()
-        self.space_button = KeyboardButton.space()
-        base.accept("escape",sys.exit)
-        # movement speed
-        self.forward_speed = 5.0 # units per second
-        self.backward_speed = 2.0
-        # UI
-        menu = DirectOptionMenu(text="New", scale=0.04,items=["Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"],highlightColor=(0.65,0.65,0.65,1),command=self.BookLoad)
-        menuold = DirectOptionMenu(text="Old", scale=0.04,items=["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalms","Proverbs","Ecclesiastes","SongofSolomon","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi"],highlightColor=(0.65,0.65,0.65,1),command=self.BookLoad)
-        menu.setPos(-1.31,0,0.8)
-        menuold.setPos(-1.31,0,0.9)
-        DownButton = DirectButton(text = (">"), scale=.15,command=self.GoDown)
-        DownButton.setHpr(0,0,90)
-        DownButton.setPos(-1.28,0,-0.9)
-        UpButton = DirectButton(text = ("<"), scale=.15, command=self.GoUp)
-        UpButton.setHpr(0,0,90)
-        UpButton.setPos(1.24,0,-0.9)
-        base.setBackgroundColor(1,1,1,1)
-        # movement task
-        taskMgr.add(self.MovementTask, "MyTaskName")
-    def ClearText(self):
-        self.TextHolder.removeNode()
-        self.TextHoldingNode = TextNode("node name")
-        self.TextHoldingNode.setText("")
-        self.TextHolder = aspect2d.attachNewNode(self.TextHoldingNode)
-        self.TextHolder.reparentTo(render)
-        self.TextHoldingNode.setTextColor(0,0,0,1)
-    def BookLoad(self,arg):
-        self.ClearText()
-        base.trackball.node().setPos(-7.55,0,6)
-        textfile = open(str(arg) + ".txt")
-        self.hi = 1
-        for line in textfile.read().splitlines():
-                self.text = TextNode("node name")
-                self.text.setText(line)
-                textNodePath = aspect2d.attachNewNode(self.text)
-                textNodePath.setScale(0.44)
-                textNodePath.setPos(0,30,self.hi)
-                textNodePath.reparentTo(self.TextHolder)
-                self.text.setTextColor(0,0,0,1)
-                self.hi = self.hi - 0.5
-    def GoDown(self):
-        base.trackball.node().setPos(-7.55, 0, base.trackball.node().getZ() + 3)
-    def GoUp(self):
-        base.trackball.node().setPos(-7.55, 0, base.trackball.node().getZ() - 3)
-    def MovementTask(self,task):
-        base.trackball.node().setPos(-7.55, 0, base.trackball.node().getZ())
-        base.trackball.node().setHpr(0, 0, 0)
-        speed = 0.0
-        is_down = base.mouseWatcherNode.is_button_down
-        if is_down(self.forward_button):
-            speed += self.forward_speed
-            y_delta = -5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        if is_down(self.backward_button):
-            speed -= self.backward_speed
-            y_delta = 5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        if is_down(self.space_button):
-            speed -= self.backward_speed
-            y_delta = 5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        return task.cont
+        self.text = TextNode("node name")
+        self.BibleTextFileName = "eng-kjv2006_vpl.txt"
+        self.CurrentBibleBook = "" 
+        # init text
+        self.textNodePath = aspect2d.attachNewNode(self.text)
+        self.textNodePath.setScale(0.11)
+        self.textNodePath.setPos(0,0,0)
+        self.text.setTextColor(0,0,0,1)
+       # base.setBackgroundColor(1,1,1)
+        self.text.setWordwrap(30)
+        # new menu stuff
+        self.BibleBookListFrame = DirectScrolledFrame(canvasSize=(0, 0, -7, 7), frameSize=(-.1, .2, -1.5, 1.5))
+        self.BibleBookListFrame.setPos(-1.2, 0, -0)
+        self.BibleBookListFrame.setScale(0.6)
+        #load the books into the menu
+        self.LoadBibleBookMenu()
+        #Bible Frame
+        #initialize it so it can be used as a variable
+        self.BibleFrame = DirectScrolledFrame()
+        #load up the bible frame
+        self.LoadBibleFrame()
+        # Chapter menu
+        self.BibleChapterMenu = DirectScrolledFrame()
+        # Load John 1
+        self.LoadBibleBook("JOH",1)
+    def LoadBibleFrame(self):
+        self.BibleFrame.removeNode()
+        self.BibleFrame = DirectScrolledFrame(canvasSize=(0, 0, -20, 20), frameSize=(-1.7, 1.7, -1.5, 1.5))
+        #self.BibleFrame.setPos(-1.2, 0, -0)
+        self.BibleFrame.setScale(0.6)
+        self.textNodePath.setPos(-0,0,19.9)
+        self.textNodePath.reparentTo(self.BibleFrame.getCanvas())
+    def LoadBibleBookMenu(self):
+        origlist = [] 
+        textfile = open(self.BibleTextFileName)
+        for line in textfile:
+                linestring = line[:4]
+                origlist.append(linestring)
+                #print linestring
+        resultlist = [] 
+        for i in origlist: 
+                 if i not in resultlist: 
+                      resultlist.append(i)
+        zoffset = 6.9          
+        for book in resultlist:
+            l = DirectButton(text=book, scale=.1, command=self.LoadBibleBook,extraArgs=[book,1])
+            l.setZ(zoffset)
+            zoffset = zoffset -0.13
+            l.setX(0.1)
+            l.reparentTo(self.BibleBookListFrame.getCanvas())
+    def LoadChapterMenu(self, arg):
+        self.BibleChapterMenu.removeNode()
+        self.BibleChapterMenu = DirectScrolledFrame(canvasSize=(0, 0, -10, 10), frameSize=(-.1, .2, -1.5, 1.5))
+        self.BibleChapterMenu.setPos(1.2, 0, -0)
+        self.BibleChapterMenu.setScale(0.6)
+        textfile = open(self.BibleTextFileName)
+        for line in textfile:
+             if line.startswith(str(arg)):
+                 pass
+                 last_line = line
+        new = last_line.split(":")
+        # print new[0]
+        newstring = new[0]
+        finalnumstr = newstring[4:]
+        self.chapternumber = int(finalnumstr)
+        zoffset = 9.9         
+        for x in range(self.chapternumber + 1):
+            if x != 0:
+                l = DirectButton(text=str(x), scale=.1, command=self.LoadBibleBook,extraArgs=[arg.strip(),x])
+                l.setZ(zoffset)
+                zoffset = zoffset -0.13
+                l.setX(0.1)
+                l.reparentTo(self.BibleChapterMenu.getCanvas())
+    def LoadBibleBook(self,arg,chapter):
+        #reload Bible frame. 
+        self.LoadBibleFrame()
+        # set chapter number to 1
+        self.LoadChapterMenu(arg)
+        booktext = ""
+        textfile = open(self.BibleTextFileName)
+        for line in textfile:
+            if line.startswith(str(arg.strip()) + " " + str(chapter) +":"):
+                   #remove VPL prefix
+                   booktext = booktext + line[4:] 
+        self.text.setText(booktext)
 fgame = Game()
 base.run()
